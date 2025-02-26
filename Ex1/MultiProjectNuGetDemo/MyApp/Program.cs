27cce5d533f0183e6c7d06a843693d0a9c8e0e18
ZuzannaZawartka <raf.zuzia7@gmail.com>
2025-02-27 00:35:30 +0100

    feat(task_1): add task to folder

---
+++ Ex1/MultiProjectNuGetDemo/MyApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyLibrary;
+using MyServices;
+using Newtonsoft.Json;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//             Calculator calc = new Calculator();
+//             int sum = calc.Add(5, 3);
+//             var result = new { Operation = "Add", A = 5, B = 3, Result = sum };
+//             string jsonResult = JsonConvert.SerializeObject(result, Formatting.Indented);
+//             Console.WriteLine(jsonResult);
+   
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<ILoggerService, ConsoleLogger>()
+                .BuildServiceProvider();
+
+           
+            var logger = serviceProvider.GetService<ILoggerService>();
+            logger.Log("Aplikacja uruchomiona.");
+
+
+            int sum = new Calculator().Add(10, 15);
+            logger.Log($"Wynik dodawania: {sum}");
+        }
+    }
+}
---
+++ Ex1/MultiProjectNuGetDemo/MyLibrary/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace MyLibrary
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Substract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
